3b358a800a5617bf8cccd9049abaeda152a86946
kharioki <antolisah@gmail.com>
2021-02-07 04:56:12 +0300

    define server

--- index.js
+++ index.js
@@ -18,3 +18,36 @@ const schema = gql`
     User(id: ID!): User
   }
 `;
+
+const users = [
+    {
+        id: '1',
+        name: 'Tony',
+    },
+];
+
+const pages = [
+    {
+        id: '1',
+        name: 'Hello World',
+        author: users[0],
+        content: 'Lorem ipsum...',
+    },
+];
+
+const resolvers = {
+    Query: {
+      Page: (_, args) => {
+        return pages.find(page => page.id === args.id);
+      },
+      User: (_, args) => {
+        return users.find(user => user.id === args.id);
+      },
+    },
+};
+
+const server = new ApolloServer({ typeDefs: schema, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 GraphQL server started at: ${url}`);
+});
